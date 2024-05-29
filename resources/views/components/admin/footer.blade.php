08bc70850c73907222bd0118bcc958e235c809eb
rdevacc <rdevacc@gmail.com>
2024-05-29 11:53:22 +0700

    feat: Fixing header display responsive

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -4,6 +4,6 @@
       &copy; Copyright <strong><span>BSIPMektan</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      Designed by <a href="#">Muhammad Rizky A M</a>
     </div>
   </footer><!-- End Footer -->
