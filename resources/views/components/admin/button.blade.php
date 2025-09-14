b12b6920c7e3205497afe25ba99aed97beb5caf2
rdevacc <r.devacc@gmail.com>
2025-09-14 07:20:14 +0700

    feat: Hidding Show Button

--- resources/views/components/admin/button.blade.php
+++ resources/views/components/admin/button.blade.php
@@ -1,8 +1,8 @@
 <div class="d-flex">
-    <a class="btn btn-info" href="{{ route('activity.show', $model->id) }}" data-bs-toggle="tooltip" data-bs-placement="top"
+    {{-- <a class="btn btn-info" href="{{ route('activity.show', $model->id) }}" data-bs-toggle="tooltip" data-bs-placement="top"
         data-bs-custom-class="custom-tooltip" data-bs-title="Lihat Detail">
         <i class="bi bi-eye"></i>
-    </a>
+    </a> --}}
     <a class="btn btn-warning mx-1" href="{{ route('activity.edit', $model->id) }}" data-bs-toggle="tooltip"
         data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Edit Pengaduan">
         <i class="bi bi-pencil"></i>
