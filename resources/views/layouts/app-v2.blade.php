2157f700c54dcacbe78f88becd17068665960182
rdevacc <r.devacc@gmail.com>
2025-09-14 07:24:09 +0700

    feat: Fixing title App Blade

--- resources/views/layouts/app-v2.blade.php
+++ resources/views/layouts/app-v2.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Dashboard - E-Arsip BRMP Mektan</title>
+    <title>Dashboard - E-Monev BRMP Mektan</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
