eb51741e24ed76688151bc2fe58ff712811a4215
rdevacc <r.devacc@gmail.com>
2026-01-21 13:59:50 +0700

    feat: Update Web Routes for Reverse Proxy Migration

--- routes/web.php
+++ routes/web.php
@@ -33,113 +33,123 @@ Route::get('/', function(){
     return redirect()->route('login');
 });
 
-/**
- * * Authentication Route *
- */
-Route::get('/v2/app/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
-Route::post('/v2/app/login', [LoginController::class, 'authenticate'])->name('login-submit');
-Route::post('/v2/app/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/v2/app/forgot-password', [ForgotPasswordController::class, 'forgot_password'])->middleware('guest')->name('forgot-password');
-Route::post('/v2/app/forgot-password-submit', [ForgotPasswordController::class, 'forgot_password_submit'])->name('forgot-password-submit');
-
-Route::get('/v2/app/reset-password/{token}', [ResetPasswordController::class, 'reset_password'])->middleware('guest')->name('password.reset');
-Route::post('/v2/app/reset-password/', [ResetPasswordController::class, 'reset_password_submit'])->middleware('guest')->name('password.update');
-
-
-Route::prefix('/v2')->middleware('auth')->group(function () {
+Route::prefix('/v2')->group(function() {
 
     /**
-     * * Dashboard Route
+     * * Guest Routes Only *
      */
-    Route::get('/app/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    
-    /**
-     * * Usage Guide Route
-     */
-    Route::get('/guide/pj', [UsageGuideController::class, 'pj'])->name('guide.pj');
-    Route::get('/guide/admin', [UsageGuideController::class, 'admin'])->name('guide.admin');
-    Route::get('/guide/pdf/{filename}', [UsageGuideController::class, 'showPdf'])->name('guide.pdf');
+    Route::middleware('guest')->group(function() {
+        /**
+         * * Authentication Route *
+         */
+        Route::get('/v2/app/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
+        Route::post('/v2/app/login', [LoginController::class, 'authenticate'])->name('login-submit');
+        Route::post('/v2/app/logout', [LoginController::class, 'logout'])->name('logout');
 
-    /**
-     * * Excel Route
-     */
-    Route::get('/app/export', [ActivityExportController::class, 'index'])->name('export.index');
-    Route::get('/app/export-excel', [ActivityExportController::class, 'exportExcel'])->name('export.excel');
+        Route::get('/v2/app/forgot-password', [ForgotPasswordController::class, 'forgot_password'])->middleware('guest')->name('forgot-password');
+        Route::post('/v2/app/forgot-password-submit', [ForgotPasswordController::class, 'forgot_password_submit'])->name('forgot-password-submit');
 
-    /**
-     * * PDF Route
-     */
-    // Route::get('/app/kegiatan/page-pdf', [PDFController::class, 'pagePDF'])->name('page-pdf');
-    Route::post('/app/kegiatan/pdf', [PDFController::class, 'generatePDF'])->name('pdf');
+        Route::get('/v2/app/reset-password/{token}', [ResetPasswordController::class, 'reset_password'])->middleware('guest')->name('password.reset');
+        Route::post('/v2/app/reset-password/', [ResetPasswordController::class, 'reset_password_submit'])->middleware('guest')->name('password.update');
 
+    });
 
     /**
-     * * User Route
+     * * Auth Routes Only *
      */
-    Route::resource('/app/user', UserController::class)->names([
-        'index' => 'user.index',
-        'create' => 'user.create',
-        'store' => 'user.create-submit',
-        'edit' => 'user.edit',
-        'update' => 'user.edit-submit',
-        'destroy' => 'user.delete',
-    ])->middleware('admin');
+    Route::middleware('auth')->group(function() {
+        /**
+         * * Dashboard Route
+         */
+        Route::get('/app/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        
+        /**
+         * * Usage Guide Route
+         */
+        Route::get('/guide/pj', [UsageGuideController::class, 'pj'])->name('guide.pj');
+        Route::get('/guide/admin', [UsageGuideController::class, 'admin'])->name('guide.admin');
+        Route::get('/guide/pdf/{filename}', [UsageGuideController::class, 'showPdf'])->name('guide.pdf');
 
-    /**
-     * * Role Route
-     */
-    Route::resource('/app/role', RoleController::class)->names([
-        'index' => 'role.index',
-        'create' => 'role.create',
-        'store' => 'role.create-submit',
-        'edit' => 'role.edit',
-        'update' => 'role.edit-submit',
-        'destroy' => 'role.delete',
-    ]);
+        /**
+         * * Excel Route
+         */
+        Route::get('/app/export', [ActivityExportController::class, 'index'])->name('export.index');
+        Route::get('/app/export-excel', [ActivityExportController::class, 'exportExcel'])->name('export.excel');
 
-    /**
-     * * Work Group Route
-     */
-    Route::resource('/app/work-group', WorkGroupController::class)->names([
-        'index' => 'work-group.index',
-        'create' => 'work-group.create',
-        'store' => 'work-group.create-submit',
-        'edit' => 'work-group.edit',
-        'update' => 'work-group.edit-submit',
-        'destroy' => 'work-group.delete',
-    ]);
+        /**
+         * * PDF Route
+         */
+        // Route::get('/app/kegiatan/page-pdf', [PDFController::class, 'pagePDF'])->name('page-pdf');
+        Route::post('/app/kegiatan/pdf', [PDFController::class, 'generatePDF'])->name('pdf');
 
-    /**
-     * * Work Team Route
-     */
-    Route::resource('/app/work-team', WorkTeamController::class)->names([
-        'index' => 'work-team.index',
-        'create' => 'work-team.create',
-        'store' => 'work-team.create-submit',
-        'edit' => 'work-team.edit',
-        'update' => 'work-team.edit-submit',
-        'destroy' => 'work-team.delete',
-    ]);
 
-    /**
-     * * Activity Route
-     */
-    Route::resource('/app/activity', ActivityController::class)->names([
-        'index' => 'activity.index',
-        'create' => 'activity.create',
-        'show' => 'activity.show',
-        'store' => 'activity.create-submit',
-        'edit' => 'activity.edit',
-        'update' => 'activity.edit-submit',
-        'destroy' => 'activity.delete',
-    ]);
+        /**
+         * * User Route
+         */
+        Route::resource('/app/user', UserController::class)->names([
+            'index' => 'user.index',
+            'create' => 'user.create',
+            'store' => 'user.create-submit',
+            'edit' => 'user.edit',
+            'update' => 'user.edit-submit',
+            'destroy' => 'user.delete',
+        ])->middleware('admin');
 
-    Route::get('/app/activity/{id}/monthly-data', [ActivityController::class, 'getMonthlyData'])
-    ->name('activity.monthly-data');
-    Route::get('/app/activity/{id}/show-monthly-data', [ActivityController::class, 'getShowMonthlyData'])
-    ->name('activity.show-monthly-data');
+        /**
+         * * Role Route
+         */
+        Route::resource('/app/role', RoleController::class)->names([
+            'index' => 'role.index',
+            'create' => 'role.create',
+            'store' => 'role.create-submit',
+            'edit' => 'role.edit',
+            'update' => 'role.edit-submit',
+            'destroy' => 'role.delete',
+        ]);
 
-    Route::post('/app/activity/{activity}/clear-monthly', [ActivityController::class, 'clearMonthlyData'])->name('activity.clearMonthlyData');
+        /**
+         * * Work Group Route
+         */
+        Route::resource('/app/work-group', WorkGroupController::class)->names([
+            'index' => 'work-group.index',
+            'create' => 'work-group.create',
+            'store' => 'work-group.create-submit',
+            'edit' => 'work-group.edit',
+            'update' => 'work-group.edit-submit',
+            'destroy' => 'work-group.delete',
+        ]);
 
+        /**
+         * * Work Team Route
+         */
+        Route::resource('/app/work-team', WorkTeamController::class)->names([
+            'index' => 'work-team.index',
+            'create' => 'work-team.create',
+            'store' => 'work-team.create-submit',
+            'edit' => 'work-team.edit',
+            'update' => 'work-team.edit-submit',
+            'destroy' => 'work-team.delete',
+        ]);
+
+        /**
+         * * Activity Route
+         */
+        Route::resource('/app/activity', ActivityController::class)->names([
+            'index' => 'activity.index',
+            'create' => 'activity.create',
+            'show' => 'activity.show',
+            'store' => 'activity.create-submit',
+            'edit' => 'activity.edit',
+            'update' => 'activity.edit-submit',
+            'destroy' => 'activity.delete',
+        ]);
+
+        Route::get('/app/activity/{id}/monthly-data', [ActivityController::class, 'getMonthlyData'])
+        ->name('activity.monthly-data');
+        Route::get('/app/activity/{id}/show-monthly-data', [ActivityController::class, 'getShowMonthlyData'])
+        ->name('activity.show-monthly-data');
+
+        Route::post('/app/activity/{activity}/clear-monthly', [ActivityController::class, 'clearMonthlyData'])->name('activity.clearMonthlyData');
+    });
 });
